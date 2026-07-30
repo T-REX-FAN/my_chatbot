b429188f1187d928f7b326883f3622d6ca047b98
T-REX-FAN <raon1588a@gmail.com>
2026-07-30 18:58:02 +0900

    Update app_chatbot.py

--- app_chatbot.py
+++ app_chatbot.py
@@ -2,3 +2,11 @@ import streamlit
 
 print("hello, world")
 streamlit.write("Hi, world")
+streamlit.write("1")
+streamlit.write("2")
+streamlit.write("3")
+streamlit.write("4")
+streamlit.write("5")
+streamlit.write("6")
+streamlit.write("123456")
+streamlit.write("1,2,3,4,5,6")
