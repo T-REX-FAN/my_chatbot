89a1ea8a03ba96f5208914d70e2b493e56b4fea0
T-REX-FAN <raon1588a@gmail.com>
2026-07-30 19:00:28 +0900

    Update app_chatbot.py

--- app_chatbot.py
+++ app_chatbot.py
@@ -1,12 +1,6 @@
 import streamlit
 
 print("hello, world")
-streamlit.write("Hi, world")
-streamlit.write("1")
-streamlit.write("2")
-streamlit.write("3")
-streamlit.write("4")
-streamlit.write("5")
-streamlit.write("6")
-streamlit.write("123456")
-streamlit.write("1,2,3,4,5,6")
+for number in range:(1,6):
+  print(number)
+  streamlit.write(number)
