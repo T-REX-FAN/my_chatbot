be6c0f8a7fe89acd7f33d2b934b2c4e7802df999
T-REX-FAN <raon1588a@gmail.com>
2026-08-06 18:17:42 +0900

    Refactor chatbot input and greeting functionality
    Comment out the initial print and loop statements. Add input for name and display a greeting.

--- app_chatbot.py
+++ app_chatbot.py
@@ -1,6 +1,10 @@
-import streamlit
+ import streamlit
 
-print("hello, world")
-for number in range(1,6):
-  print(number)
-  streamlit.write(number)
+# print("hello, world")
+# for number in range(1,6):
+#   print(number)
+#   streamlit.write(number)
+
+input("이름을 입력하세요")
+name = st.text_input("이름을 입력하세요")
+st.write(f"{name}님 반갑습니다")
