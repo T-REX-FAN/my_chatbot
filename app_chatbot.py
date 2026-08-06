05908c83e370795825ae0b1b1c6348854477a0f9
T-REX-FAN <raon1588a@gmail.com>
2026-08-06 18:54:56 +0900

    Refactor chatbot interaction and button logic

--- app_chatbot.py
+++ app_chatbot.py
@@ -5,11 +5,7 @@ import streamlit as st
 #   print(number)
 #   streamlit.write(number)
 
-st.button("눌러보세요")
-
 st.title("날씨 챗봇")
-
-st.header("반갑습니다")
 
 st.subheader("오늘은 어떠신가요?")
 
@@ -22,11 +18,13 @@ with col1:
 
   if name:
     st.write(f"{age}살, {name}님 반갑습니다")
+    st.header(f"{name}님 반갑습니다")
+    st.button("나이를 여쭈어봐도 될까요?")
     # st.write(type(age))
 
 with col2:
 
-  if name:
+  if name and st.button("나이를 여쭈어봐도 될까요?") :
 
     option = st.selectbox("날씨를 선택하세요",["맑음","흐림","비","폭우","지구종말"])
     st.write(option)
