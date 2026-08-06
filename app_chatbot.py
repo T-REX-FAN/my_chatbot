be79031e219003bb1d074d668d3ba4280753ca44
T-REX-FAN <raon1588a@gmail.com>
2026-08-06 18:42:58 +0900

    Refactor greeting logic in app_chatbot.py
    Removed duplicate greeting message and type display.

--- app_chatbot.py
+++ app_chatbot.py
@@ -12,10 +12,13 @@ with col1:
   name = st.text_input("이름을 입력하세요")
   age = st.number_input("나이를 입력하세요")
 
+  if name:
+    st.write(f"{age}살, {name}님 반갑습니다")
+    # st.write(type(age))
+
 with col2:
 
   if name:
-    st.write(f"{age}살, {name}님 반갑습니다")
-    st.write(type(age))
+
     option = st.selectbox("날씨를 선택하세요",["맑음","흐림","비","폭우","지구종말"])
     st.write(option)
