ed0c9ec7f2db8f17feae4c8270909ca54b71233d
T-REX-FAN <raon1588a@gmail.com>
2026-08-06 18:40:59 +0900

    Update app_chatbot.py

--- app_chatbot.py
+++ app_chatbot.py
@@ -6,11 +6,16 @@ import streamlit as st
 #   streamlit.write(number)
 
 # input("이름을 입력하세요")
-name = st.text_input("이름을 입력하세요")
-age = st.number_input("나이를 입력하세요")
+col1, col2 = st.columns(2)
 
-if name:
-  st.write(f"{age}살, {name}님 반갑습니다")
-  st.write(type(age))
-  option = st.selectbox("날씨를 선택하세요",["맑음","흐림","비","폭우","지구종말"])
-  st.write(option)
+with col1
+  name = st.text_input("이름을 입력하세요")
+  age = st.number_input("나이를 입력하세요")
+
+with col2
+
+  if name:
+    st.write(f"{age}살, {name}님 반갑습니다")
+    st.write(type(age))
+    option = st.selectbox("날씨를 선택하세요",["맑음","흐림","비","폭우","지구종말"])
+    st.write(option)
