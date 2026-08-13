90f50ca3b9766aca375ce52425b223ce7b6c3865
T-REX-FAN <raon1588a@gmail.com>
2026-08-13 18:50:34 +0900

    Integrate weather API for current weather information
    Added weather API integration to fetch and display weather information based on user input coordinates.

--- app_chatbot.py
+++ app_chatbot.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import requests
 
 # print("hello, world")
 # for number in range(1,6):
@@ -27,3 +28,35 @@ with col2:
 
     option = st.selectbox("날씨를 선택하세요",["맑음","흐림","비","폭우","지구종말"])
     st.write(option)
+
+
+SERVICE_KEY = "U%2F3cxgpV%2Bn4fLqOHggb3q0Wzbc2DliDoXqNP4FR5yMD7XMsUdxu6n%2FRS6ymOtyqIBJVC2ddPcwdOrHvX%2B%2FgBvQ%3D%3D"
+BASE_URL = "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0"
+
+nx = st.text_input("지역의 X좌표를 입력하세요 (예:60")
+ny = st.text_input("지역의 Y좌표를 입력하세요 (예:60")
+
+if st.button("날씨 확인"):
+  params = {
+    'serviceKey' : SERVICE_KEY,
+    'pageNo': '1',
+    'numOfRows': '10',
+    'dataType': 'JSON',
+    'base_date': '2026813',
+    'base_time': '0700',
+    'nx': nx,
+    'ny': ny,
+  }
+
+response = request.get(BASE_URL, params=params)
+data = response.json()
+
+try:
+  items = data['response']['body']['item']
+  st.write('##현재 날씨 정보')
+  for item in items:
+    category = item['category']
+    value = item['fcsValue']
+    st.write(f"- {category}: {value}")
+except KeyError:
+  st.error("데이터를 가져올수없습니다.")
