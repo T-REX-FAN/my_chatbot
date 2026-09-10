9211523a8954df26bd8b06605112971bba824fbc
T-REX-FAN <raon1588a@gmail.com>
2026-09-10 18:57:48 +0900

    Map city names and translate weather conditions
    Updated weather display to use translated weather conditions.

--- op_weather_.py
+++ op_weather_.py
@@ -6,21 +6,25 @@ API_KEY = "8306a95ad41f685d2e59c0e448102948"
 st.title("우리동네 날씨챗봇")
 st.write("도시 이름을 입력하면 현재 날씨를 알려드려요")
 city = st.text_input("도시 이름을 영어로 입력하세요")
+city_map = {"서울":"Seoul", "부산":"Busan", "인천":"Inchen", "대구":"Daegu", "광주":"Gwangju"}
+weather_dict = {"Clouds":"흐림", "Clear":"맑음", "Rain":"비", "Snow":"눈", "Mist":"안개"}
 def get_weather(city_name):
   url = f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}&units=metric&lang=kr"
   response = requests.get(url)
   #st.write(response.json())
   return response.json()
 
+
 # city = st.text_input("도시 이름을 영어로 입력하세요")
 
 if city:
+  Eng_city = city_map[city]
   #st.write(city)
-  weather_data = get_weather(city)
+  weather_data = get_weather(Eng_city)
   st.write(weather_data)
 
   st.write(f"{city}의 날씨를 알려준다")
-  st.write(f"날씨는 : {weather_data['weather'][0]['main']}")
+  st.write(f"날씨는 : weather_dict[{weather_data['weather'][0]['main']}]")
   st.write(f"온도는 : {int(weather_data['main']['temp'])} C")
   st.write(f"최저온도는 : {int(weather_data['main']['temp_min'])} C")
   st.write(f"최고온도는 : {int(weather_data['main']['temp_max'])} C")
