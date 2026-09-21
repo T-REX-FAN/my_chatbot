99c5e5e4da5a5e38a09dacf2bab10cfb6777d67a
T-REX-FAN <raon1588a@gmail.com>
2026-09-21 20:16:53 +0900

    Round temperature values in weather display

--- op_weather_.py
+++ op_weather_.py
@@ -25,6 +25,6 @@ if city:
 
   st.write(f"{city}의 날씨를 알려준다")
   st.write(f"날씨는 : weather_dict[{weather_data['weather'][0]['main']}]")
-  st.write(f"온도는 : {round_half_up(weather_data['main']['temp'])} C")
-  st.write(f"최저온도는 : {round_half_up(weather_data['main']['temp_min'])} C")
-  st.write(f"최고온도는 : {round_half_up(weather_data['main']['temp_max'])} C")
+  st.write(f"온도는 : {round(weather_data['main']['temp'])} C")
+  st.write(f"최저온도는 : {round(weather_data['main']['temp_min'])} C")
+  st.write(f"최고온도는 : {round(weather_data['main']['temp_max'])} C")
